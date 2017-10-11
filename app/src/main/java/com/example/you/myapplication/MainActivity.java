6f22bb1ee176ea1d6fdcae3aeb0b3a87382762f5
YouSenRong <1178899349@qq.com>
2017-10-12 00:36:50 +0800

    commit change

--- app/src/main/java/com/example/you/myapplication/MainActivity.java
+++ app/src/main/java/com/example/you/myapplication/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.you.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import java.util.*;
 
